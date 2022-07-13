f466e47e71b38200ff23d84ef0373953941920b1
KseniaBastrakova <Bastrakova.Kseinia@gmail.com>
2022-07-13 14:20:42 +0200

    add check of sig storage

--- main.cpp
+++ main.cpp
@@ -10,27 +10,25 @@
 #include <cstdlib>
 #include <iostream>
 
-struct Function
-{
-    template<typename TAcc>
-    ALPAKA_FN_ACC auto operator()(TAcc const& acc, float const x) -> float
-    {
-        return alpaka::math::sqrt(acc, (1.0f - x * x));
-    }
-};
+using Dim = alpaka::DimInt<1>;
 
 struct Kernel
 {
     template<typename TAcc>
     ALPAKA_FN_ACC auto operator()(
         TAcc const& acc,
-		mxmc::Particle* const particles) const -> void
+		mxmc::SIG* const ptrHostParticles) const -> void
     {
         // Get the global linearized thread idx.
         auto const globalThreadIdx = alpaka::getIdx<alpaka::Grid, alpaka::Threads>(acc);
         auto const globalThreadExtent = alpaka::getWorkDiv<alpaka::Grid, alpaka::Threads>(acc);
 
         auto const linearizedGlobalThreadIdx = alpaka::mapIdx<1u>(globalThreadIdx, globalThreadExtent)[0];
+
+        if (linearizedGlobalThreadIdx == 1)
+        {
+				ptrHostParticles[1].get_fission();
+        }
 
         auto engine = alpaka::rand::engine::createDefault(
             acc,
@@ -40,8 +38,7 @@ struct Kernel
         auto dist(alpaka::rand::distribution::createUniformReal<float>(acc));
 
         float random_value = dist(engine);
-        if (random_value > 0.5)
-			particles[linearizedGlobalThreadIdx].weight = 0;
+
 
     }
 };
@@ -64,57 +61,50 @@ double calculate_weights(mxmc::Particle* const ptrHostParticles, size_t num_part
 auto main() -> int
 {
 
+
     using Dim = alpaka::DimInt<1>;
     using Idx = std::size_t;
     using Vec = alpaka::Vec<Dim, Idx>;
-    using Acc = alpaka::ExampleDefaultAcc<Dim, Idx>;
+   // using Acc = alpaka::ExampleDefaultAcc<Dim, Idx>;
     using Host = alpaka::DevCpu;
-    auto const devAcc = alpaka::getDevByIdx<Acc>(0u);
-    auto const devHost = alpaka::getDevByIdx<Host>(0u);
+    using Acc = alpaka::AccCpuSerial<Dim, Idx>;
+    std::cout << "Using alpaka accelerator: " << alpaka::getAccName<Acc>() << std::endl;
+    auto const dev_host = alpaka::getDevByIdx<Host>(0u);
+    auto const dev_acc = alpaka::getDevByIdx<Acc>(0u);
+
+
     using QueueProperty = alpaka::Blocking;
     using QueueAcc = alpaka::Queue<Acc, QueueProperty>;
-    QueueAcc queue{devAcc};
+    QueueAcc queue{dev_acc};
 
-    using BufHostParticles = alpaka::Buf<Host, mxmc::Particle, Dim, Idx>;
-    using BufAccParticles = alpaka::Buf<Acc, mxmc::Particle, Dim, Idx>;
+    Idx number_groups = 10;
+
+
+    mxmc::SIG_storage test_storage(number_groups, dev_acc, dev_host);
+
     using WorkDiv = alpaka::WorkDivMembers<Dim, Idx>;
 
     constexpr size_t numThreads = 100u;
     constexpr size_t numAlpakaElementsPerThread = 1;
 
-
     WorkDiv workdiv{alpaka::getValidWorkDiv<Acc>(
-        devAcc,
+    		dev_acc,
         Vec(numThreads),
         Vec(numAlpakaElementsPerThread),
         false,
         alpaka::GridBlockExtentSubDivRestrictions::Unrestricted)};
 
+    auto device_sig = test_storage.get_pointer_host();
+    for (int i = 0; i < number_groups; i++)
+    	device_sig[i].set_fission(12.2);
 
-    constexpr size_t number_particles = 100u;
-    BufHostParticles bufHostParticles{alpaka::allocBuf<mxmc::Particle, Idx>(devHost, number_particles)};
-    mxmc::Particle* const ptrBufHostParticles{alpaka::getPtrNative(bufHostParticles)};
+    test_storage.copy_from_host_to_device(queue);
+    auto pointer_to_device = test_storage.get_pointer_device();
 
-    BufAccParticles bufAccParticles{alpaka::allocBuf<mxmc::Particle, Idx>(devAcc, number_particles)};
-    mxmc::Particle* const ptrBufAccParticles{alpaka::getPtrNative(bufAccParticles)};
-
-
-    simpleParticlesInitialisation(ptrBufHostParticles, number_particles);
-    alpaka::memcpy(queue, bufAccParticles, bufHostParticles);
-
-
-
-
-    double sum_weights_before = calculate_weights(ptrBufHostParticles, number_particles);
     Kernel kernel;
-    alpaka::exec<Acc>(queue, workdiv, kernel, ptrBufAccParticles);
-    alpaka::memcpy(queue, bufHostParticles, bufAccParticles);
-    alpaka::wait(queue);
+    alpaka::exec<Acc>(queue, workdiv, kernel, pointer_to_device);
+    test_storage.copy_from_device_to_host(queue);
 
-    double sum_weights_after = calculate_weights(ptrBufHostParticles, number_particles);
 
-    std::cout << "sum weights before "<< sum_weights_before<< "\n";
-    std::cout << "sum weights after "<< sum_weights_after << "\n";
-    std::cout << "relative difference "<< sum_weights_before /sum_weights_after << "\n";
     return EXIT_SUCCESS;
 }
